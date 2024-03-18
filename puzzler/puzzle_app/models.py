9c4bc14b8f3f94cdbc7c4539a05ac2becac78ad4
YourPalMike <74157166+PrimeStrat@users.noreply.github.com>
2024-03-17 22:55:23 -0400

    Models Removed

--- puzzler/puzzle_app/models.py
+++ puzzler/puzzle_app/models.py
@@ -2,20 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# API TO USE LATER: https://opentdb.com/api.php?amount=1
-
-class Quiz(models.Model):
-    title = models.CharField(max_length=255)
-
-class Question(models.Model):
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-    is_correct = models.BooleanField(default=False)
-
 class Player(models.Model):
     name = models.CharField(max_length=255)
     score = models.IntegerField(default=0)
